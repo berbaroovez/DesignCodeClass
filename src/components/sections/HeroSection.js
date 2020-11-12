337e0f0be2a238e6eb85990f503ffd40dda45b76
berbaroovez <carterolson3@gmail.com>
2020-11-11 23:08:08 -0600

    day two of design code

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -2,6 +2,8 @@ import React from "react"
 import styled from "styled-components"
 import { H1, MediumText } from "../styles/TextStyles"
 import {themes} from "../styles/ColorStyles"
+import PurchaseButton from "../buttons/PurchaseButton"
+import MockupAnimation from "../animations/MockupAnimation"
 export default function HeroSection() {
   return (
     <>
@@ -16,7 +18,12 @@ export default function HeroSection() {
               Don’t skip design. Learn design and code, by building real apps,
               collaborate and solving product UI problems.
             </Description>
+            <PurchaseButton
+              title="Start learning"
+              subtitle = "120+ hours of video"
+            />
           </TextWrapper>
+          <MockupAnimation/>
         </ContentWrapper>
       </Wrapper>
     </>
@@ -25,11 +32,15 @@ export default function HeroSection() {
 
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+  overflow: hidden;
 `
 const ContentWrapper = styled.div`
+  position:relative;
   max-width: 1234px;
   margin: 0 auto;
   padding: 200px 30px;
+  display:grid;
+  grid-template-columns:500px auto;
 `
 
 const TextWrapper = styled.div`
